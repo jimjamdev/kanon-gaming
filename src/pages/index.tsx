72b201f21e6d57b830544a629957f3e77f0c5370
Ian Jamieson <ian@jimjam.dev>
2023-02-16 18:58:03 +0100

    add manifest.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,13 @@ export default function Home(): JSX.Element {
           content="My Kanon is loaded and ready to fire!"
           name="description"
         />
+        <meta content="#2E7BBA" name="theme-color" />
         <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <link href="/manifest.json" rel="manifest" />
+        <link
+          href="/images/icons/apple-touch-icon.png"
+          rel="apple-touch-icon"
+        />
         <link href="/favicon.ico" rel="icon" />
       </Head>
       <h1 className="font-bold bg-orange-500 text-fuchsia-50 uppercase p-2.5 text-4xl text-center">
