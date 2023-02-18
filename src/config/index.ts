898d01368d9e2b1f020728e513d503b4095448f7
Ian Jamieson <ian@jimjam.dev>
2023-02-18 19:31:15 +0100

    fix config for build

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,9 @@ const env = process.env;
 
 export const config = {
   apiUrl: `${
-    env.VERCEL_URL || env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+    env.VERCEL_URL ||
+    env.API_URL ||
+    env.NEXT_PUBLIC_API_URL ||
+    'http://localhost:3000'
   }/api/`,
 };
