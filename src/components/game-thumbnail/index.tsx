3bcffac561231ad39466b00842d1b04224528b57
Ian Jamieson <cogocreative@gmail.com>
2023-02-20 01:41:31 +0100

    game page fix

--- src/components/game-thumbnail/index.tsx
+++ src/components/game-thumbnail/index.tsx
@@ -15,7 +15,7 @@ export function GameThumbnail({
     <div className="flex flex-col items-center justify-center">
       <h4>{title}</h4>
       {providerName}
-      <Image alt={title || ''} height={280} priority src={thumb?.url ? `https:${thumb.url}` : '/images/vercel.svg'} width={280} />
+      <Image alt={title || ''} height={280} priority src={thumb?.url ? `https:${thumb.url}` : '/vercel.svg'} width={280} />
       {slug}
     </div>
   );
