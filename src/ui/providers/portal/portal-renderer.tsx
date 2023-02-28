5e1be99b67ee3b389a49332cd6e7cacfe0aa9cc8
Ian Jamieson <cogocreative@gmail.com>
2023-02-28 05:14:17 +0100

    mon night updates

--- src/ui/providers/portal/portal-renderer.tsx
+++ src/ui/providers/portal/portal-renderer.tsx
@@ -1,20 +1,14 @@
-import type { ComponentType } from 'react';
 import { usePortal } from '@/ui/providers/portal/hook';
 import type { TPortal } from '@/ui/providers/portal/context';
 import { Portal } from '@/ui/components/portal';
 
-export function PortalRenderer({
-  portalsList,
-}: {
-  portalsList: ComponentType;
-}) {
+export function PortalRenderer({ portalsList }: { portalsList: any }) {
   const { portals = [], closePortal } = usePortal();
   return (
     <>
       {portals
         ? portals?.map((portal: TPortal) => {
             const { portalKey = '' } = portal;
-            // @ts-expect-error - type
             const PortalComponent = portalsList[portalKey];
             return (
               <Portal isOpen key={portalKey}>
