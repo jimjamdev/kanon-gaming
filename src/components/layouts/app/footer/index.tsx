9ed70f22afbf8a1bff9251772de03b461e98ac5a
Ian Jamieson <cogocreative@gmail.com>
2023-03-01 22:11:04 +0100

    missing aria tags

--- src/components/layouts/app/footer/index.tsx
+++ src/components/layouts/app/footer/index.tsx
@@ -15,25 +15,37 @@ export function Footer({ className }: { className?: string }) {
       <div className="flex justify-between w-full bg-white text-white p-4">
         <Link href="/">
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="text-gray-500 hover:text-gray-400">
+          <a
+            aria-label="Go to the home page"
+            className="text-gray-500 hover:text-gray-400"
+          >
             <HomeIcon className="w-8 h-8" />
           </a>
         </Link>
         <Link href="/games">
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="text-gray-500 hover:text-gray-400">
+          <a
+            aria-label="Browse our games"
+            className="text-gray-500 hover:text-gray-400"
+          >
             <PuzzlePieceIcon className="w-8 h-8" />
           </a>
         </Link>
         <Link href="/promotions">
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="text-gray-500 hover:text-gray-400">
+          <a
+            aria-label="Win more with our promotions"
+            className="text-gray-500 hover:text-gray-400"
+          >
             <BanknotesIcon className="w-8 h-8" />
           </a>
         </Link>
         <Link href="/">
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="text-gray-500 hover:text-gray-400">
+          <a
+            aria-label="Update your user profile"
+            className="text-gray-500 hover:text-gray-400"
+          >
             <UserCircleIcon className="w-8 h-8" />
           </a>
         </Link>
